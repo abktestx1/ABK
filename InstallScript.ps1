ab0600dc4246664bd6fca88abbf4cf0672f736cb
abktestx1 <75633859+abktestx1@users.noreply.github.com>
2020-12-07 18:45:41 +0100

    Update InstallScript.ps1

--- InstallScript.ps1
+++ InstallScript.ps1
@@ -1,1 +1,11 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install anydesk.install -y
+choco install googlechrome -y
 choco install firefox -y
+choco install ccleaner -y
+choco install adobereader -y
+choco install 7zip -y
+choco install javaruntime -y
+choco install pdf24 -y
+choco install classic-shell -y
+
