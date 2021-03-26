a7286e19b33e781e7dfd6da6880cd738c7b3b158
abktestx1 <75633859+abktestx1@users.noreply.github.com>
2021-03-26 15:28:39 +0100

    Update InstallScript.ps1

--- InstallScript.ps1
+++ InstallScript.ps1
@@ -14,10 +14,25 @@ Set-Service -Name "SSDPSRV" -StartupType Automatic
 Restart-Service -Force -Name "SSDPSRV"
 Set-Service -Name "upnphost" -StartupType Automatic
 Restart-Service -Force -Name "upnphost"
+#Rometdesktopverbindung aktivieren
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+#Firewall deaktivieren
 netsh advfirewall set allprofiles state off
+#Energiesparplan Höchstleistung erstellen und auswählen
 powercfg -SETACTIVE 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+#IPv6 für alle Adapter deaktivieren
 Disable-NetAdapterBinding -Name "*" -ComponentID ms_tcpip6
+#Cortana Deinstallieren
 Get-AppxPackage *Microsoft.549981C3F5F10* | Remove-AppxPackage
+#Erweiterte Freigabe Einstellungen aktivieren
 Set-NetFirewallRule -DisplayGroup "Datei- und Druckerfreigabe" -Enabled True
 Set-NetFirewallRule -DisplayGroup "Netzwerkerkennung" -Enabled True
+#Telemetrie deaktivieren Win Version Allgemein 
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
+Disable-ScheduledTask -TaskName "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" | Out-Null
+Disable-ScheduledTask -TaskName "Microsoft\Windows\Autochk\Proxy" | Out-Null
+Disable-ScheduledTask -TaskName "Microsoft\Windows\Customer Experience Improvement Program\Consolidator" | Out-Null
+Disable-ScheduledTask -TaskName "Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" | Out-Null
+Disable-ScheduledTask -TaskName "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" | Out-Null
